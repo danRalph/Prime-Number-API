8edc1c90ff338794c0cdbec3dd94fd51f19b3d79
danRalph <dan.r.richards@hotmail.co.uk>
2021-11-24 19:58:15 +0000

    Prime

--- Frontend/src/Primes.test.js
+++ Frontend/src/Primes.test.js
@@ -1,15 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Primes from './Primes';
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Primes />, div);
 });
 
 it('renders input text', () => {
-  render(<App />);
+  render(<Primes />);
   expect(screen.getByText('Input a number:')).toBeInTheDocument();
 });
 
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Primes from './Primes';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Primes />
   </React.StrictMode>,
   document.getElementById('root')
 );
